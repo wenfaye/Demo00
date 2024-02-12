6f5c1eee2a27be54e19a1c4013fea03fb76c174c
wenfaye <thesims4@126.com>
2024-02-12 19:55:57 +0800

    edit

---
+++ umi3Example/src/.umi/core/history.ts
@@ -0,0 +1,21 @@
+// @ts-nocheck
+import { createBrowserHistory, History } from '/Volumes/文件/工作区01/Demo02/umi3Example/node_modules/umi/node_modules/@umijs/runtime';
+
+let options = {
+  "basename": "/"
+};
+if ((<any>window).routerBase) {
+  options.basename = (<any>window).routerBase;
+}
+
+// remove initial history because of ssr
+let history: History = process.env.__IS_SERVER ? null : createBrowserHistory(options);
+export const createHistory = (hotReload = false) => {
+  if (!hotReload) {
+    history = createBrowserHistory(options);
+  }
+
+  return history;
+};
+
+export { history };
---
+++ umi3Example/src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from '/Volumes/文件/工作区01/Demo02/umi3Example/node_modules/react-helmet';
