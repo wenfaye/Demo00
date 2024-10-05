0f9ae1584b330294eb8df50be90ede3000abcd8f
wenfaye <thesims4@126.com>
2024-10-05 13:27:33 +0800

    test

--- umi4Example/.umirc.ts
+++ umi4Example/.umirc.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "umi";
+import routes from './config/routes'
 
 export default defineConfig({
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
-  ],
+  // routes: [
+  //   { path: "/", component: "index" },
+  //   { path: "/docs", component: "docs" },
+  // ],
+  routes,
   npmClient: 'yarn',
 });
